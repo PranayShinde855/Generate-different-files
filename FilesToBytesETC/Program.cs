c677632c3361ffe1c0234094e03c93d6403391b0
pranayshinde855 <pranayshinde855@gmail.com>
2023-02-22 21:45:40 +0530

    Convert file to bytes

---
+++ FilesToBytesETC/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace FilesToBytesETC
+{
+    class Program
+    {
+        public static byte[] ConvertFileToBytes()
+        {
+            var path = @"D:\Excel - Copy.xls";
+            //var path = Directory.GetFiles(path1);
+            FileStream fs = new FileStream(path, FileMode.Open,FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+            long numbytes = new FileInfo(path).Length;
+            byte[] buffer = br.ReadBytes((int)numbytes);
+            return buffer;
+        }
+        static void Main(string[] args)
+        {
+            var b = ConvertFileToBytes();
+            foreach (var item in b)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine("File byte created.");
+        }
+    }
+}
--- Genearting excel file in console application/Program.cs
+++ Genearting excel file in console application/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.IO.Compression;
 using Microsoft.Office.Interop;
 
@@ -61,22 +62,39 @@ namespace Genearting_excel_file_in_console_application
                 GC.Collect();
             }
         }
-
         public static void GenerateZipFile()
         {
             // Provide the file path 
             string fileToZip = @"D:/Test";
+            // Path an dName of Zip file to save
             string locationToSaveZipWithZipFileName = @"D:/Test.Zip";
-
-
             //Call the ZipFile.CreateFromDirectory
             ZipFile.CreateFromDirectory(fileToZip, locationToSaveZipWithZipFileName);
+            Console.WriteLine("Zip file created.");
+        }
+        public static void GenerateZipFileWithPassword()
+        {
+            // Provide the file path 
+            string fileToZip = @"D:/Test";
+            // Path an dName of Zip file to save
+            string locationToSaveZipWithZipFileName = @"D:/Test.Zip";
+
+            string streamFile = string.Empty;
+
+            //var fs = File.Open(fileToZip, FileMode.Open);
+            var fs = Directory.GetFiles(fileToZip);
+            Stream stream = File.OpenWrite(streamFile);
+            fs.CopyTo(stream);
+
+            ZipArchive zip = new ZipArchive(stream);
+            zip.CreateEntry(locationToSaveZipWithZipFileName);
             Console.WriteLine("Zip file created.");
         }
         static void Main(string[] args)
         {
             //GenerateExcel();
-            GenerateZipFile();
+            //GenerateZipFile();
+            GenerateZipFileWithPassword();
             Console.ReadKey();
         }
     }
